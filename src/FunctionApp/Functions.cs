b3cd66d89de8a662707f840d7c17f430e6e03a90
Anthony Chu <antchu@microsoft.com>
2018-09-20 10:48:43 -0700

    Initial commit

---
+++ src/FunctionApp/Functions.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+namespace ServerlessTrivia
+{
+    public static class Functions
+    {
+        private const string containerName = "orlando";
+
+        [FunctionName(nameof(FlightsOrchestrator))]
+        public static async Task FlightsOrchestrator(
+            [OrchestrationTrigger] DurableOrchestrationContext context,
+            ILogger logger)
+        {
+            const int maxIterations = 200;
+            var iteration = context.GetInput<int>();
+            if (iteration >= maxIterations)
+            {
+                iteration = 0;
+            }
+
+            await context.CallActivityAsync(nameof(GetAndSendData), new IterationInput
+            {
+                Iteration = iteration,
+                DataFileName = $"{containerName}/{iteration}.json"
+            });
+
+            if (iteration < maxIterations) 
+            {
+                context.ContinueAsNew(iteration + 1);
+            }
+        }
+
+        [FunctionName(nameof(GetAndSendData))]
+        public static async Task GetAndSendData(
+            [ActivityTrigger] DurableActivityContext context,
+            [SignalR(HubName = "flights")] IAsyncCollector<SignalRMessage> signalRHub,
+            ILogger logger)
+        {
+            var input = context.GetInput<IterationInput>();
+
+            var storageAccount = CloudStorageAccount.Parse(
+                Environment.GetEnvironmentVariable("AzureWebJobsStorage", EnvironmentVariableTarget.Process));
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(containerName);
+            var blob = container.GetBlobReference($"{input.Iteration}.json");
+            using (var blobStream = await blob.OpenReadAsync())
+            using (var streamReader = new StreamReader(blobStream))
+            {
+                var jsonTextReader = new JsonTextReader(streamReader);
+                var data = JsonSerializer.CreateDefault().Deserialize<Data>(jsonTextReader);
+
+                var flightData = new
+                {
+                    time = data.Time,
+                    flights = data.States
+                        .Select(f => new
+                        {
+                            id = f[0],
+                            callsign = f[1],
+                            longitude = f[5],
+                            latitude = f[6],
+                            altitude = f[7],
+                            heading = f[10]
+                        })
+                };
+
+                await signalRHub.AddAsync(new SignalRMessage
+                {
+                    Target = "newFlightData",
+                    Arguments = new[] { flightData }
+                });
+            }
+        }
+
+        [FunctionName(nameof(HttpStartSingle))]
+        public static async Task<HttpResponseMessage> HttpStartSingle(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestMessage req,
+            [OrchestrationClient] DurableOrchestrationClient starter,
+            TraceWriter log)
+        {
+            const string instanceId = "1";
+            // Check if an instance with the specified ID already exists.
+            var existingInstance = await starter.GetStatusAsync(instanceId);
+            if (existingInstance == null)
+            {
+                await starter.StartNewAsync(nameof(FlightsOrchestrator), instanceId);
+                log.Info($"Started orchestration with ID = '{instanceId}'.");
+                return starter.CreateCheckStatusResponse(req, instanceId);
+            }
+            else
+            {
+                // An instance with the specified ID exists, don't create one.
+                return req.CreateErrorResponse(
+                    HttpStatusCode.Conflict,
+                    $"An instance with ID '{instanceId}' already exists.");
+            }
+        }
+
+        [FunctionName(nameof(SignalRInfo))]
+        public static IActionResult SignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequestMessage req,
+            [SignalRConnectionInfo(HubName = "flights")]SignalRConnectionInfo info,
+            ILogger logger)
+        {
+            return info != null
+                ? (ActionResult)new OkObjectResult(info)
+                : new NotFoundObjectResult("Failed to load SignalR Info.");
+        }
+    }
+
+    public class IterationInput
+    {
+        public int Iteration { get; set; }
+        public string DataFileName { get; set; }
+    }
+
+    public class Data
+    {
+        public int Time { get; set; }
+        public List<List<object>> States { get; set; } = new List<List<object>>();
+    }
+}
---
+++ src/GetData/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GetData
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 6)
+            {
+                Console.WriteLine("Needs command line arguments: iterations minLatitude minLongitude maxLatitude maxLongitude outputDir");
+                return;
+            }
+
+            var maxIterations = Convert.ToInt16(args[0]);
+            var minLatitude = args[1];
+            var minLongitude = args[2];
+            var maxLatitude = args[3];
+            var maxLongitude = args[4];
+            var outDir = args[5];
+
+            var httpClient = new HttpClient();
+
+            for (var i = 0; i < maxIterations; i++)
+            {
+                var path = Path.Combine(outDir, i + ".json");
+                using (var response = await httpClient.GetStreamAsync($"https://opensky-network.org/api/states/all?lamin={minLatitude}&lomin={minLongitude}&lamax={maxLatitude}&lomax={maxLongitude}"))
+                using (Stream file = File.Create(path))
+                {
+                    await response.CopyToAsync(file);
+                    file.Close();
+                    Console.WriteLine(i);
+                }
+
+                await Task.Delay(12000);
+            }
+        }
+    }
+
+    public class Data
+    {
+        public int time { get; set; }
+        public List<List<object>> states { get; set; }
+    }
+}
